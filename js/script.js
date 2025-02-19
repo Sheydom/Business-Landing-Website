9aac1ddbedac741c91b2f49d6eb3414832be98e0
Dom <dominic.knabe@gmail.com>
2025-02-19 21:59:00 +1100

    added all comments

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+
+// eventlistener once DOM loaded do function then fetch 
+// nav component then convert to text,string then take 
+// converted data and put in getelemntiD inner.HTML
+// error handling in case of error display error message
+
 document.addEventListener("DOMContentLoaded",()=>{
     fetch("/components/navbar.html")
     .then(response => response.text())
