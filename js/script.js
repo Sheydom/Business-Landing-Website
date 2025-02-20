5f8f29f1d11f70eca517a934739df08b60c6470d
Dom <dominic.knabe@gmail.com>
2025-02-20 20:10:46 +1100

    js

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded",()=>{
 
 // here i added another component to reuse the same footer element through all pages 
 document.addEventListener("DOMContentLoaded",()=>{
-    fetch("components/footer.html")
+    fetch("../components/footer.html")
     .then(response=> response.text())
     .then(data=>{
         document.getElementById("footer").innerHTML=data;
